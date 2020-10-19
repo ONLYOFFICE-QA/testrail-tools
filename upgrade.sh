e6c917c85b7f0bb690b12c975112ee5f3a1c8f62
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-10-19 18:37:37 +0300

    Add script for testrail upgrade (#3)
    * Add script for update
    
    * Rename scripts

---
+++ upgrade.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DOWNLOAD_DIR=/tmp/testrail_install
+mkdir -pv ${DOWNLOAD_DIR}
+wget https://secure.gurock.com/downloads/testrail/testrail-latest-ion71.zip -P ${DOWNLOAD_DIR}
+unzip ${DOWNLOAD_DIR}/testrail-*.zip -d ${DOWNLOAD_DIR}
+sudo cp -r ${DOWNLOAD_DIR}/testrail /var/www/html/
+rm -rf ${DOWNLOAD_DIR}
