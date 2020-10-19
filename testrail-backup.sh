c5785570c7d42202c386b961dacb059de20f3d7b
Pavel Lobashov <shockwavenn@gmail.com>
2020-10-19 16:36:18 +0300

    Add backup script

---
+++ testrail-backup.sh
@@ -0,0 +1,15 @@
+DB_BASE='testrail'
+
+DATE=`date '+%Y-%m-%d-%H-%M-%S'`
+BACKUP_NAME=testrail-backup-$DATE.tar.gz
+BACKUP_DIR=/var/backups/testrail
+
+mkdir -pv $BACKUP_DIR
+mysqldump --max-allowed-packet=32M $DB_BASE | gzip -c > $BACKUP_DIR/testrail.sql.gz
+cp -rp /var/www/html/testrail/ $BACKUP_DIR/testrail
+cp -rp /opt/testrail/attachments/ $BACKUP_DIR/attachments
+cp -rp /opt/testrail/reports/ $BACKUP_DIR/reports
+tar -zcvf $BACKUP_NAME $BACKUP_DIR
+aws s3 cp $BACKUP_NAME s3://nct-testrail-backup/
+rm $BACKUP_NAME
+rm -rf $BACKUP_DIR
